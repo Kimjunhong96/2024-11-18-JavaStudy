358c747a63523e859ccfb8ccf440130c4afe121f
sist-116 <sist-116@DESKTOP-KG7DQ47>
2024-12-23 12:51:40 +0900

    20241223

---
+++ Chapter10_라이브러리(java.util_Collection)_1/src/com/sist/main/ArrayList_정리.java
@@ -0,0 +1,42 @@
+package com.sist.main;
+/*
+ *   멜론 / 지니뮤직
+ *   ------------- 따로 저장
+ *   => JOIN => 교집합
+ *   => UNION => 합집합
+ *   => MINUS => 차집합 
+ *   -----------------
+ *   
+ *   컬렉션 : 배열의 단점을 보완
+ *          고정 => 가변
+ *          => 모든 개발자가 동이랗ㄴ 메소드를 이용 => 표준화 
+ *          => 가독성 / 처리 기능이 편리하다 
+ *   ------
+ *   List : ArrayList / Vector / LinkedList
+ *      => 순서가 가지고 있다 (인덱스)
+ *      => 데이터의 중복을 허용
+ *   Set : HashSet / TreeSet
+ *      => 순서가 없다
+ *      => 데이터 중복을 허용하지 않는다 
+ *      => 중복 제거 
+ *   Map : HashMap / Hashtable 
+ *     => 두개를 동시에 저장 
+ *        (키,값) 
+ *        => 키는 중복할 수 없다 / 값을 중복이 가능 
+ *        => 웹 => 클래스 관리자 
+ *        => HttpServletRequest
+ *           HttpServletResponse
+ *           HttpSession
+ *           Cookie
+ *           ------------------- Map
+ *           => 스프링 / MyBatis 
+ *     login.jsp?id=aaa&pwd=1234
+ */
+public class ArrayList_정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
